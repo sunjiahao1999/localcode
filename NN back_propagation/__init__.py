623d59bb11c46b25b403f33c3a90a817db8b26fa
sunjiahao1999 <578431509@qq.com>
2020-12-20 23:06:24 +0800

    反向传播算法2，完成推导

--- NN back_propagation/__init__.py
+++ NN back_propagation/__init__.py
@@ -36,6 +36,8 @@ def load_data(path, tranpose=True):
 # plt.show()
 X, y_raw = load_data('ex4data1.mat', tranpose=False)
 X = np.insert(X, 0, 1, axis=1)  # (5000,401)
+# y_ = np.eye(11, k=-1)[y_raw.ravel()][:, :-1]
+# 我这样也可以
 y = OneHotEncoder(sparse=False).fit_transform(y_raw)  # (5000,10)
 
 
@@ -53,14 +55,14 @@ def sigmoid(z):
 
 def feed_forward(theta, X):
     t1, t2 = deserialize(theta)
-    a1 = X
+    a1 = X  # (5000,401)
 
-    z2 = a1 @ t1.T
+    z2 = a1 @ t1.T  # (5000,25)
     a2 = sigmoid(z2)
     a2 = np.insert(a2, 0, 1, axis=1)  # (5000,26)
 
-    z3 = a2 @ t2.T
-    h = sigmoid(z3)
+    z3 = a2 @ t2.T  # (5000,10)
+    h = sigmoid(z3)  # (5000,10)
     return a1, z2, a2, z3, h
 
 
@@ -86,8 +88,11 @@ def re_cost(theta, X, y, l=1):
     return cost(theta, X, y) + reg_t1 + reg_t2
 
 
+print(re_cost(theta, X, y))
+
+
 def gradient(theta, X, y):
-    t1, t2 = deserialize(theta)
+    t1, t2 = deserialize(theta)  # (25,401),(10,26)
     m = X.shape[0]
     delta1 = np.zeros(t1.shape)  # (25,401)
     delta2 = np.zeros(t2.shape)  # (10,26)
@@ -103,14 +108,79 @@ def gradient(theta, X, y):
 
         d3i = hi - yi  # (10，)
 
-        d2i = (t2.T @ d3i) * a2i * (1 - a2i)  # (26,) 目前这里是最难的，不知道首项是怎么来的
+        d2i = (t2.T @ d3i) * a2i * (1 - a2i)  # (26,) 目前这里是最难的，终于会推导了
 
-        delta2 += d3i[:, None] @ a2i[None, :]
-        delta1 += d2i[1:, None] @ a1i[None, :]
+        delta2 += d3i[:, None] @ a2i[None, :]  # (10,26)
+        delta1 += d2i[1:, None] @ a1i[None, :]  # (25,401)
     delta1 = delta1 / m
     delta2 = delta2 / m
     return serialize(delta1, delta2)
 
 
+def re_gradient(theta, X, y, l=1):
+    """don't regularize theta of bias terms"""
+    m = X.shape[0]
+    delta1, delta2 = deserialize(gradient(theta, X, y))
+    t1, t2 = deserialize(theta)
+
+    t1[:, 0] = 0
+    reg_term_d1 = (l / m) * t1
+    delta1 = delta1 + reg_term_d1
+
+    t2[:, 0] = 0
+    reg_term_d2 = (l / m) * t2
+    delta2 = delta2 + reg_term_d2
+
+    return serialize(delta1, delta2)
+
+
+# 我自己改写的求梯度方法，与使用上面函数得到的结果一致
+def my_gradiant(theta, X, y):
+    t1, t2 = deserialize(theta)  # (25,401),(10,26)
+    m = X.shape[0]
+    delta1 = np.zeros(t1.shape)  # (25,401)
+    delta2 = np.zeros(t2.shape)  # (10,26)
+    a1, z2, a2, a3, h = feed_forward(theta, X)  # a1(5000,401) a2(5000,26) h(5000,10)
+    d3 = h - y  # (5000,10)
+    d2 = (d3 @ t2) * a2 * (1 - a2)  # (5000,26)
+    delta2 = (d3.T @ a2) / m  # (10,26)
+    delta1 = (d2[:, 1:].T @ a1) / m  # (25,401)
+    return serialize(delta1, delta2)
+
+
+d1_, d2_ = deserialize(my_gradiant(theta, X, y))
 d1, d2 = deserialize(gradient(theta, X, y))
 print(d1.shape, d2.shape)
+
+
+def expand_array(arr):
+    return np.ones(arr.shape[0])[:, None] @ arr[None, :]
+
+
+def gradient_checking(theta, X, y, epsilon, regularized=False):
+    def a_numeric_grad(plus, minus, regularized=False):
+        """calculate a partial gradient with respect to 1 theta"""
+        if regularized:
+            return (re_cost(plus, X, y) - re_cost(minus, X, y)) / (epsilon * 2)
+        else:
+            return (cost(plus, X, y) - cost(minus, X, y)) / (epsilon * 2)
+
+    theta_matrix = expand_array(theta)  # expand to (10285, 10285)
+    epsilon_matrix = np.identity(len(theta)) * epsilon
+
+    plus_matrix = theta_matrix + epsilon_matrix
+    minus_matrix = theta_matrix - epsilon_matrix
+
+    # calculate numerical gradient with respect to all theta
+    numeric_grad = np.array([a_numeric_grad(plus_matrix[i], minus_matrix[i], regularized)
+                             for i in range(len(theta))])
+
+    # analytical grad will depend on if you want it to be regularized or not
+    analytic_grad = re_gradient(theta, X, y) if regularized else gradient(theta, X, y)
+
+    diff = np.linalg.norm(numeric_grad - analytic_grad) / np.linalg.norm(numeric_grad + analytic_grad)
+
+    print(
+        '''If your backpropagation implementation is correct,
+        the relative difference will be smaller than 10e-9 (assume epsilon=0.0001).
+        Relative Difference: {}'''.format(diff))
---
+++ torch_learning.py
@@ -0,0 +1,30 @@
+import torch
+
+# torch.manual_seed(0)
+# torch.cuda.manual_seed(0)
+# # print(torch.__version__)
+# x = torch.empty(5, 3)
+# # print(x)
+# x = torch.rand(5, 3)
+# # print(x)
+# x = torch.zeros(5, 3, dtype=torch.long)
+# x = torch.randn(5, 3)
+# x.t_()
+# y = x[0, :]
+# y += 1
+# print(y)
+# print(x[0,:])
+# print(x)
+# x_cp=x.clone().reshape(15)
+# print(x_cp)
+# x = torch.tensor([[1., 1.], [2., 3.]])
+# y = x.numpy()
+# print(x, y)
+x = torch.ones(2, 2, requires_grad=True)
+# print(x)
+# print(x.grad_fn)
+y = x + 2
+# print(y)
+z = y * y * 3
+out = z.mean()
+print(z.is_leaf)
